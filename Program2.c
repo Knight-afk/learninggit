35636d40b902bce104695b6ff7ab64cc9dff75e0
kuber <imcoolkuber@gmail.com>
2021-01-09 22:02:05 +0530

    Please enter the commit message for your changes. Lines starting wq!
    wq!

--- Program2.c
+++ Program2.c
@@ -3,18 +3,20 @@
 int main()
 
 //sum square of two num & square sum of two num &
-
+// now i am again changing
 {
- int a,b,c,d;
+ int m,a,b,c,d;
  float e;
  printf("Enter any two  integer number : \n");
  scanf("%d%d",&a,&b);
 c=(a+b)*(a+b);
 d=a*a + b*b;
 e=cbrt(a+b);
+m=sqrt(c);
 printf("\nsum square : %d",c);
 printf("\nsquare sum : %d",d);
-printf("\nsquare root of a+b : %f",e);
+printf("\ncube root of a+b : %f",e);
+printf("\nsquare root of sumsquare : %d",m);
     getch();
 
 }
